b1eb379ec740b3b1d28da6749946a4c427b4efcf
k-chaney <chaneyk@seas.upenn.edu>
2025-01-05 10:38:48 -0500

    Migrate folder to pygrocy2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/sebrut/pygrocy",
+    url="https://github.com/k-chaney/pygrocy2",
     license="MIT",
     packages=setuptools.find_packages(),
     install_requires=[
